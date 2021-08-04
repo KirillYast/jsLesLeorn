3fe66d864d09e99ef074191fe962114ea386f43e
KirillYast <Yastrebov.kirill@mail.ru>
2021-08-04 10:58:43 +0500

    Lesson-1

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+let j = 20;
+function tTf() {
+  let Tc = prompt("Какая температура по Целсию?", 0);
+  let Tf = (9 / 5) * Tc + 32;
+  alert("По фарингейту: " + Tf + "°");
+}
+function vasya() {
+  let name = prompt("Name?", "Name");
+  let admin = name;
+  alert("Admin name: " + admin);
+}
+if (true) console.log(j);
